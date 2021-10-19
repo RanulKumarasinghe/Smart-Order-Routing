8679182b8f5c968f69ee59abe9bae00bb6262c91
Ranul Kumarasinghe <45975597+RanulKumarasinghe@users.noreply.github.com>
2021-10-19 11:38:13 +0100

    Initial workspace for project
    Includes folders for entities, controllers etc.

---
+++ SpringSmartOrderRouting/src/main/java/com/ab/SpringSmartOrderRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.ab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSmartOrderRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSmartOrderRoutingApplication.class, args);
+	}
+
+}
